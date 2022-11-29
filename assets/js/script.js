c8bdd6762be6caf2b59152590b02dd44280a39d2
jts272 <jts272@outlook.com>
2022-11-29 23:46:43 +0000

    docs: add docstring in js for modal script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+/**
+ * Get the elements required to open, close and display the 'How to Play' modal.
+ * Arrow functions are used in the event listener to show and close the modal.
+ * Guide consulted: https://www.youtube.com/watch?v=TAB_v6yBXIE
+ */
+
 const howto = document.querySelector(".howto-modal");
 const openHowto = document.querySelector("#open-howto");
 const closeHowto = document.querySelector(".close-howto-modal");
