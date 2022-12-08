17a78b4e92fbec2304c510ee0cc6d688f374f34e
jts272 <jts272@outlook.com>
2022-12-08 19:28:36 +0000

    feat: get medium game elements and create 2 questions

--- assets/js/game_medium.js
+++ assets/js/game_medium.js
@@ -1,1 +1,32 @@
-alert("hello from game_medium")
+/* jshint esversion: 6 */
+
+// Get elements
+
+let qNum = document.querySelector("#question-number");
+let qOf = document.querySelector("#question-of");
+
+let score = document.querySelector("#score");
+
+let choiceA = document.querySelector(".btn-a");
+let choiceB = document.querySelector(".btn-b");
+let choiceC = document.querySelector(".btn-c");
+
+let next = document.querySelector(".next");
+
+let questionsMedium = [
+  {
+    question: 
+    "What initials are on the coffin at the beginning of Thunderball?",
+    answerA: "A.C",
+    answerB: "E.L",
+    answerC: "J.B",
+    correct: "C"
+  },
+  {
+    question: "What was the first Bond film NOT based on a book?",
+    answerA: "Moonraker",
+    answerB: "The Spy Who Loved Me",
+    answerC: "You Only Live Twice",
+    correct: "B"
+  }
+];
