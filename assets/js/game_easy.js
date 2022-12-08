a4711692afc834e4b1514eefeac0314b2eeac342
jts272 <jts272@outlook.com>
2022-12-08 16:27:28 +0000

    style: adjust easy game script for jshint warnings

--- assets/js/game_easy.js
+++ assets/js/game_easy.js
@@ -1,3 +1,6 @@
+/* jshint esversion:6 */
+/* jshint multistr: true */
+
 // Guide referenced in creation of this quiz:
 // https://simplestepscode.com/javascript-quiz-tutorial/
 
@@ -10,52 +13,52 @@
 // The questions used for the easy quiz:
 
 const questionsEasy = [{
-    question: "The James Bond movies are based on the novels by which British\
-  author?",
-    answers: {
-      A: "J. R. R. Tolkien",
-      B: "Ian Fleming",
-      C: "George Orwell"
-    },
-    correctAnswer: "B"
+  question: "The James Bond movies are based on the novels by which British\
+author?",
+  answers: {
+    A: "J. R. R. Tolkien",
+    B: "Ian Fleming",
+    C: "George Orwell"
   },
-  {
-    question: "Which artist sang the theme song 'Skyfall'?",
-    answers: {
-      A: "Adele",
-      B: "Sam Smith",
-      C: "Shirley Bassey"
-    },
-    correctAnswer: "A"
+  correctAnswer: "B"
+},
+{
+  question: "Which artist sang the theme song 'Skyfall'?",
+  answers: {
+    A: "Adele",
+    B: "Sam Smith",
+    C: "Shirley Bassey"
   },
-  {
-    question: "Which secret service agency does 007 work for?",
-    answers: {
-      A: "NCA",
-      B: "MI6",
-      C: "CIA"
-    },
-    correctAnswer: "B"
+  correctAnswer: "A"
+},
+{
+  question: "Which secret service agency does 007 work for?",
+  answers: {
+    A: "NCA",
+    B: "MI6",
+    C: "CIA"
   },
-  {
-    question: "Which actor made his first appearance as James Bond in 1995's\
-  'GoldenEye'?",
-    answers: {
-      A: "Sean Bean",
-      B: "Pierce Brosnan",
-      C: "Sean Connery"
-    },
-    correctAnswer: "B"
+  correctAnswer: "B"
+},
+{
+  question: "Which actor made his first appearance as James Bond in 1995's\
+'GoldenEye'?",
+  answers: {
+    A: "Sean Bean",
+    B: "Pierce Brosnan",
+    C: "Sean Connery"
   },
-  {
-    question: "What was the first James Bond movie?",
-    answers: {
-      A: "Goldfinger",
-      B: "Casino Royale",
-      C: "Dr. No"
-    },
-    correctAnswer: "C"
-  }
+  correctAnswer: "B"
+},
+{
+  question: "What was the first James Bond movie?",
+  answers: {
+    A: "Goldfinger",
+    B: "Casino Royale",
+    C: "Dr. No"
+  },
+  correctAnswer: "C"
+}
 ];
 
 // 'Get' elements used in the quiz to store as variables
@@ -65,91 +68,91 @@ let resultsContainer = document.getElementById('results');
 let submitButton = document.getElementById('submit');
 
 /** 
- * This function will generate the quiz content. It requires input from the quiz
- * questions, the container for the question and answers, the submit button and
- * a container for the results of the quiz. The main function 'createQuiz()' has
- * helper functions nested inside to perform the actions of their semantic
- * meaning.
- */
+* This function will generate the quiz content. It requires input from the quiz
+* questions, the container for the question and answers, the submit button and
+* a container for the results of the quiz. The main function 'createQuiz()' has
+* helper functions nested inside to perform the actions of their semantic
+* meaning.
+*/
 
 generateQuiz(questionsEasy, quizContainer, resultsContainer, submitButton);
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-  function showQuestions(questions, quizContainer) {
-    // Vars to store the output and answer choices
-    let output = [];
-    let answers;
+function showQuestions(questions, quizContainer) {
+  // Vars to store the output and answer choices
+  let output = [];
+  let answers;
 
-    // For loop to address each question
-    for (let i = 0; i < questions.length; i++) {
+  // For loop to address each question
+  for (let i = 0; i < questions.length; i++) {
 
-      // Reset the list of answers
-      answers = [];
+    // Reset the list of answers
+    answers = [];
 
-      // For each available answer
-      for (letter in questions[i].answers) {
+    // For each available answer
+    for (let letter in questions[i].answers) {
 
-        // Add a radio button in HTML
-        answers.push(
-          '<label>' +
-          '<input type="radio" name="question' + i + '" value="' + letter + '">'
-          + letter + ': ' + questions[i].answers[letter] + '</label>'
-        );
-      }
-
-      // Add the question and its answers to the output
-      output.push(
-        '<div class="question">' + questions[i].question + '</div>' +
-        '<div class="answers">' + answers.join(' ') + '</div>'
-      );
+      // Add a radio button in HTML
+      if ( answers.push(
+        '<label>' +
+        '<input type="radio" name="question' + i + '" value="' + letter +
+        '">' + letter + ': ' + questions[i].answers[letter] + '</label>'
+      ));
     }
 
-    // Combine the output list into on string of HTML
-    quizContainer.innerHTML = output.join(' ');
+    // Add the question and its answers to the output
+    output.push(
+      '<div class="question">' + questions[i].question + '</div>' +
+      '<div class="answers">' + answers.join(' ') + '</div>'
+    );
   }
 
-  function showResults(questions, quizContainer, resultsContainer) {
+  // Combine the output list into on string of HTML
+  quizContainer.innerHTML = output.join(' ');
+}
 
-    // Get the answer containers from the quiz
-    let answerContainers = quizContainer.querySelectorAll('.answers');
+function showResults(questions, quizContainer, resultsContainer) {
 
-    // Keep track of user's answers
-    let userAnswer = '';
-    let numCorrect = 0;
+  // Get the answer containers from the quiz
+  let answerContainers = quizContainer.querySelectorAll('.answers');
 
-    // For loop to address each question
-    for (let i = 0; i < questions.length; i++) {
+  // Keep track of user's answers
+  let userAnswer = '';
+  let numCorrect = 0;
 
-      // Find the selected answer
-      userAnswer = (answerContainers[i].querySelector('input[name=question' + i 
-      + ']:checked') || {}).value;
+  // For loop to address each question
+  for (let i = 0; i < questions.length; i++) {
 
-      // If the answer is correct
-      if (userAnswer === questions[i].correctAnswer) {
-        // Increase the counter for correct answers
-        numCorrect++;
+    // Find the selected answer
+    userAnswer = (answerContainers[i].querySelector('input[name=question' +
+    i + ']:checked') || {}).value;
 
-        // Colour them green
-        answerContainers[i].style.color = "#003d00";
-      }
-      // If the answer is wrong or not submitted
-      else {
-        // Colour them red
-        answerContainers[i].style.color = "#660000";
-      }
+    // If the answer is correct
+    if (userAnswer === questions[i].correctAnswer) {
+      // Increase the counter for correct answers
+      numCorrect++;
+
+      // Colour them green
+      answerContainers[i].style.color = "#003d00";
     }
-
-    // Show the number of correct answers out of the total questions
-    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    // If the answer is wrong or not submitted
+    else {
+      // Colour them red
+      answerContainers[i].style.color = "#660000";
+    }
   }
 
-  // Show questions function call
-  showQuestions(questions, quizContainer);
+  // Show the number of correct answers out of the total questions
+  resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
 
-  // Show the results when the submit button is clicked
-  submitButton.onclick = function () {
-    showResults(questions, quizContainer, resultsContainer);
-  }
+// Show questions function call
+showQuestions(questions, quizContainer);
+
+// Show the results when the submit button is clicked
+submitButton.onclick = function () {
+  showResults(questions, quizContainer, resultsContainer);
+};
 
 }
