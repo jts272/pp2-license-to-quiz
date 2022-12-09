c4ebf0f5f8cf27d4dc97a734f72345e60036ebd6
jts272 <jts272@outlook.com>
2022-12-09 14:50:27 +0000

    refactor: create object for game logic methods

--- assets/js/game_medium.js
+++ assets/js/game_medium.js
@@ -45,35 +45,45 @@ let singleQuestion = {
   correctAnswer: 2
 };
 
-// This function adds the event listeners for clicks only once upon game start.
-// The forEach method from the showQuestion function can be re-used to add the 
-// event listeners to each element
-function startGame() {
-  // Add event listeners to the choice buttons
-  choiceButtons.forEach(function (element, index) {
-    element.addEventListener("click", function () {
-      console.log("Check Correct Answer");
-    });
-  });
-  // showQuestion function call moved inside here for when startGame function is
-  // called instead of being called in global scope
-  showQuestion(singleQuestion);
-}
+// Create Object for game logic, where the functions become methods of the
+// object.
+let app = {
 
-// This function is 'ready to receive' the placeholder of 'q'. When the function
-// is called, I can substitute the q in the function parentheses for the actual
-// singleQuestion object as its argument. In other words, the parameters are in
-// the function definition and the arguments are in the actual call.
-function showQuestion(q) {
-  // Display the question text in the question container
-  question.textContent = q.title;
-  // Display the answer text in 'each' container. See Array.forEach(); syntax
-  choiceButtons.forEach(function (element, index) {
-    element.textContent = q.alternatives[index];
-  });
+  /*
+  This function adds the event listeners for clicks only once upon game start.
+  The forEach method from the showQuestion function can be re-used to add the 
+  event listeners to each element
+  */
+  startGame: function () {
+    // Add event listeners to the choice buttons
+    choiceButtons.forEach(function (element, index) {
+      element.addEventListener("click", function () {
+        console.log("Check Correct Answer");
+      });
+    });
+    // 'this' refers to the 'app' object. See below that showQuestion is now a
+    // method of the app object.
+    this.showQuestion(singleQuestion);
+  },
+
+  /*
+  This function is 'ready to receive' the placeholder of 'q'. When the function
+  is called, I can substitute the q in the function parentheses for the actual
+  singleQuestion object as its argument. In other words, the parameters are in
+  the function definition and the arguments are in the actual call.
+  */
+  showQuestion: function (q) {
+    // Display the question text in the question container
+    question.textContent = q.title;
+    // Display the answer text in 'each' container. See Array.forEach(); syntax
+    choiceButtons.forEach(function (element, index) {
+      element.textContent = q.alternatives[index];
+    });
+
+  }
+
 };
 
-// The actual function call. Where the function definition would have used the
-// placeholder of 'q', it will use the object assigned in the argument.
-
-startGame();
+// Start game function is now a method of the app object and is called as shown
+// here
+app.startGame();
