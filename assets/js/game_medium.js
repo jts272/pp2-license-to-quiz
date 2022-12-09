84aa6d813618f6dd8111a706bd28e07f69ce81ef
jts272 <jts272@outlook.com>
2022-12-09 13:23:32 +0000

    feat: code event listener to check answer

--- assets/js/game_medium.js
+++ assets/js/game_medium.js
@@ -11,7 +11,7 @@ let question = document.querySelector(".question");
 
 let choiceButtons = document.querySelectorAll(".answer-choice");
 let choiceButtonsArray = Array.from(choiceButtons);
-console.log(choiceButtons);
+// console.log(choiceButtons);
 
 let choiceA = document.querySelector(".btn-a");
 let choiceB = document.querySelector(".btn-b");
@@ -57,9 +57,23 @@ function showQuestion(q) {
   // Display the answer text in 'each' container. See Array.forEach(); syntax
   choiceButtons.forEach(function (element, index) {
     element.textContent = q.alternatives[index];
+    // Elements are already established with forEach so I can add the event
+    // listeners on the element paramater to add it to all of them at once
+    element.addEventListener("click", function() {
+      console.log("Clicked: " + index);
+      if (index == q.correctAnswer) {
+        console.log("Correct Answer!");
+      } else {
+        console.log("Wrong Answer!");
+      }
+    });
   });
 };
 
 // The actual function call. Where the function definition would have used the
 // placeholder of 'q', it will use the object assigned in the argument.
 showQuestion(singleQuestion);
+
+next.addEventListener("click", function(){
+  // console.log("Clicked!");
+});
