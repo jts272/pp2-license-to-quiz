04d5a6bd97fb22f1064d6996333b2f6edbe551c5
jts272 <jts272@outlook.com>
2022-12-09 00:05:47 +0000

    feat: add code to display medium quiz options

--- assets/js/game_medium.js
+++ assets/js/game_medium.js
@@ -11,6 +11,7 @@ let question = document.querySelector(".question");
 
 let choiceButtons = document.querySelectorAll(".answer-choice");
 let choiceButtonsArray = Array.from(choiceButtons);
+console.log(choiceButtons);
 
 let choiceA = document.querySelector(".btn-a");
 let choiceB = document.querySelector(".btn-b");
@@ -38,8 +39,27 @@ let questionsMedium = [
 
 // Guide: https://gamedevacademy.org/javascript-quiz-tutorial/
 
+// This single question is an object. Its index 1 is an array of choices for the
+// user.
 let singleQuestion = {
   title: "Who was the second actor to portray James Bond in a feature film?",
   alternatives: ["Roger Moore", "Timothy Dalton", "George Lazenby"],
   correctAnswer: 2
 };
+
+// This function is 'ready to receive' the placeholder of 'q'. When the function
+// is called, I can substitute the q in the function parentheses for the actual
+// singleQuestion object as its argument. In other words, the parameters are in
+// the function definition and the arguments are in the actual call.
+function showQuestion(q) {
+  // Display the question text in the question container
+  question.textContent = q.title;
+  // Display the answer text in 'each' container. See Array.forEach(); syntax
+  choiceButtons.forEach(function (element, index) {
+    element.textContent = q.alternatives[index];
+  });
+};
+
+// The actual function call. Where the function definition would have used the
+// placeholder of 'q', it will use the object assigned in the argument.
+showQuestion(singleQuestion);
